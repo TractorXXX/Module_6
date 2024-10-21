604ddf350466c15bca611235a511c3fc971bb435
Константин <tractorx@yandex.ru>
2024-10-21 20:44:41 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -23,8 +23,8 @@ class Eagle:
 class Pegasus(Horse, Eagle):
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
         return self.x_distance, self.y_distance
